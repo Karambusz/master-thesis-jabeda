6971f1e4d7bf5fbf4eea98558bf24d2f70b2b940
Vadym <vsamkovich@ukr.net>
2023-04-02 12:54:27 +0200

    added theme, spacer and fonts to app

--- mobile-app/App.js
+++ mobile-app/App.js
@@ -1,12 +1,35 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, View } from 'react-native';
+import { Text } from './src/components/typography/text'
+import { theme } from "./src/infrastructure/theme";
+import { ThemeProvider } from "styled-components/native";
+import {
+    useFonts as useOswald,
+    Oswald_400Regular,
+} from "@expo-google-fonts/oswald";
+import {
+    useFonts as useLato,
+    Lato_400Regular } from "@expo-google-fonts/lato";
 
 export default function App() {
+    const [oswaldLoaded] = useOswald({
+        Oswald_400Regular,
+    });
+
+    const [latoLoaded] = useLato({
+        Lato_400Regular,
+    });
+
+    if (!oswaldLoaded || !latoLoaded) {
+        return null;
+    }
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+      <ThemeProvider theme={theme}>
+        <View style={styles.container}>
+          <Text variant="label">Text</Text>
+          <StatusBar style="auto" />
+        </View>
+      </ThemeProvider>
   );
 }
 
