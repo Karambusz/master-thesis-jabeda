20a8b80bf97b1c0299d5c18de0e228d5b3925b60
Vadym <vsamkovich@ukr.net>
2023-04-07 17:04:27 +0200

    added zooming when taking photo

--- mobile-app/src/features/camera/screens/camera.js
+++ mobile-app/src/features/camera/screens/camera.js
@@ -1,4 +1,5 @@
 import React, { useRef, useState } from "react";
+import { PinchGestureHandler } from "react-native-gesture-handler";
 import { Camera, CameraType } from "expo-camera";
 import { CameraPreview } from "./camera-preview";
 import { Text } from "../../../components/typography/text";
@@ -8,10 +9,27 @@ import { ReportProblemCamera, TakePhotoButtonContainer, TakePhotoButton } from "
 
 export const CameraScreen = ({navigation, ...props}) => {
     const cameraRef = useRef();
+    const [zoom, setZoom] = useState(0);
     const [previewVisible, setPreviewVisible] = useState(false);
     const [capturedImage, setCapturedImage] = useState(null);
 
     const [permission, requestPermission] = Camera.useCameraPermissions();
+
+    const onPinchGestureEvent = (nativeEvent) => {
+        const scale = nativeEvent.nativeEvent.scale;
+        const velocity = nativeEvent.nativeEvent.velocity / 20;
+
+        let newZoom =
+            velocity > 0
+                ? zoom + scale * velocity * 0.01
+                : zoom -
+                scale * Math.abs(velocity) * 0.02
+
+        if (newZoom < 0) newZoom = 0;
+        else if (newZoom > 0.5) newZoom = 0.5;
+
+        setZoom(newZoom)
+    };
     const takePhoto = async () => {
         if (cameraRef) {
             const photo = await cameraRef.current.takePictureAsync();
@@ -51,15 +69,20 @@ export const CameraScreen = ({navigation, ...props}) => {
                     <CameraPreview photo={capturedImage} retakePhoto={retakePhoto} />
             )
             : (
-                <ReportProblemCamera
-                    ref={camera => (cameraRef.current = camera)}
-                    type={CameraType.back}>
-                    <TakePhotoButtonContainer>
-                        <TakePhotoButton
-                            onPress={takePhoto}
-                        />
-                    </TakePhotoButtonContainer>
-                </ReportProblemCamera>
+                <PinchGestureHandler
+                    onGestureEvent={onPinchGestureEvent}
+                >
+                    <ReportProblemCamera
+                        ref={camera => (cameraRef.current = camera)}
+                        type={CameraType.back}
+                        zoom={zoom}>
+                        <TakePhotoButtonContainer>
+                            <TakePhotoButton
+                                onPress={takePhoto}
+                            />
+                        </TakePhotoButtonContainer>
+                    </ReportProblemCamera>
+                </PinchGestureHandler>
             )
         }
         </>
